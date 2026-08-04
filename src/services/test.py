124e2290e2fd1825b445c6483337419b5a9e4bfb
Efe <efesoylemis@gmail.com>
2026-08-04 15:51:43 +0300

    I have created a CLI using argpars module. Now everything is given from the command line. Customer, product and order generation can be made seperately. order_items follows the order creation. data_generation_service class is no longer needed that's why I deleted it.

--- src/services/test.py
+++ src/services/test.py
@@ -35,11 +35,52 @@ order_item_generator = OrderItemGenerator()
 order_item_service = OrderItemGenerationService(order_item_generator,order_item_repository)
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description="CLI for Ddata Generation",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser = argparse.ArgumentParser(description="CLI for Ddata Generation")
-parser.add_argument('-c','--customer',default="aa",help="Generate customers and write them to customer table.")
-parser.add_argument('-p','--product',help="Generate products and write them to product table.")
+    parser.add_argument('-c','--customer',type=int,help="Generate customers and write them to customer table.")
+    parser.add_argument('-p','--product',type=int,help="Generate products and write them to product table.")
+    parser.add_argument('-o','--order',type=int,help="Generate orders and write them to orders table.")
+    parser.add_argument('-f','--first-generation',action='store_true',help="Use this option if the database is newly initialized.")
 
 
 
-args = parser.parse_args()
+    return parser.parse_args()
+
+
+args = get_args()
+
+customer_count = args.customer
+first_time = args.first_generation
+product_count = args.product
+order_count = args.order # This number will be provided to order. It will choose random customer_ids. Not very efficient by the way. Refactor, maybe?
+
+def customer_operation():
+    if customer_count != None:
+        customer_service.generate(customer_count)
+
+def product_operation():
+    if product_count != None:
+        product_service.generate(product_count)
+
+def order_operation(): # order_items are followed by orders creation. 
+    if order_count != None:
+        customer_ids = [] 
+        
+        for _ in range(order_count):
+            id = customer_service._repository.choose_random_customer_id()
+            customer_ids.append(id)
+
+        if first_time:
+            max_order_id = None
+        else:
+            max_order_id = order_service._repository.max_order_id() 
+
+        order_service.generate(customer_ids)
+        orders = order_service._repository.get_orders(max_order_id,first_time)
+        products = product_service._repository.select_all_products()
+        order_item_service.generate(orders,products)
+
+customer_operation()
+order_operation()
+product_operation()
