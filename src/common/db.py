c6a443d93d255079832e20dc621cc89fc92ef5ab
Efe <efesoylemis@gmail.com>
2026-08-17 17:28:45 +0300

    Added a connection function for the anayltics database.

--- src/common/db.py
+++ src/common/db.py
@@ -9,3 +9,20 @@ def get_connection():
         **DB_CONFIG,
         row_factory=dict_row
     )
+
+
+
+def get_connection_analytics():
+
+    analytics_config= {
+            "host":"localhost",
+            "port":5433,
+            "dbname":"analytics",
+            "user":"postgres",
+            "password":"postgres"
+        }
+    
+    return psycopg.connect(
+        **analytics_config,
+        row_factory=dict_row
+    )
